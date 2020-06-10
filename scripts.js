c0d02120dac7147469c57a8a1ff2275ead254357
Abraham Berlin <berlinwall31@gmail.com>
2020-06-10 12:37:59 -0500

    Added UI

--- scripts.js
+++ scripts.js
@@ -1,53 +1,56 @@
-game();
-function game() {
-    let wins = 0;
-    let = losses = 0;
-    let ties = 0;
-    while (true) {
-        let playerSelection = humanPlays();
-        let computerSelection = computerPlays();
-        let playerWin = computePlayerWins(playerSelection, computerSelection);
-        let outcome = "";
-        if (playerWin == 0) {
-            losses++
-            outcome = "lost";
-        } else if (playerWin == 1) {
-            wins++;
-            outcome = "won";
-        } else if (playerWin == 2) {
-            ties++;
-            outcome = "tied";
-        }
-        console.log("You " + outcome + ". You chose " + playerSelection + " and the computer chose " + computerSelection + ". \nYour record is: " + wins + "-" + ties + "-" + losses );
+let wins = 0;
+let ties = 0;
+let losses = 0;
 
-    }
+
+function rock() {
+    let playerSelection = "rock";
+    let computerSelection = computerPlays();
+    computePlayerWins(playerSelection, computerSelection);
 }
+
+function paper() {
+    let playerSelection = "paper";
+    let computerSelection = computerPlays();
+    computePlayerWins(playerSelection, computerSelection);
+}
+
+function scissors() {
+    let playerSelection = "scissors";
+    let computerSelection = computerPlays();
+    computePlayerWins(playerSelection, computerSelection);
+}
+
+
 function computePlayerWins(playerSelection, computerSelection) {
     if (playerSelection == "rock" && computerSelection == "paper") {
-        return 0;
+        losses++;
+        document.getElementById("losses").innerHTML = losses;
+        document.getElementById("statement").innerHTML = "You Lost! You chose " + playerSelection + " and the computer chose " + computerSelection;
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        return 1;
+        wins++;
+        document.getElementById("wins").innerHTML = wins;
+        document.getElementById("statement").innerHTML = "You Won! You chose " + playerSelection + " and the computer chose " + computerSelection;
     } else if (playerSelection == "paper" && computerSelection == "rock") {
-        return 1;
+        wins++;
+        document.getElementById("wins").innerHTML = wins;
+        document.getElementById("statement").innerHTML = "You Won! You chose " + playerSelection + " and the computer chose " + computerSelection;
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        return 0;
+        losses++;
+        document.getElementById("losses").innerHTML = losses;
+        document.getElementById("statement").innerHTML = "You Lost! You chose " + playerSelection + " and the computer chose " + computerSelection;
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        return 1;
+        wins++;
+        document.getElementById("wins").innerHTML = wins;
+        document.getElementById("statement").innerHTML = "You Won! You chose " + playerSelection + " and the computer chose " + computerSelection;
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
-        return 0;
+        losses++;
+        document.getElementById("losses").innerHTML = losses;
+        document.getElementById("statement").innerHTML = "You Lost! You chose " + playerSelection + " and the computer chose " + computerSelection;
     } else {
-        return 2;
-    }
-}
-
-function humanPlays() {
-    while (true) {
-        let play = prompt("Please enter selection (rock, paper, or scissors):").toLowerCase();
-        if (play == "rock" || play == "paper" || play == "scissors") {
-            return play;
-        } else {
-            alert("Invalid Selection");
-        }
+        ties++;
+        document.getElementById("ties").innerHTML = ties;
+        document.getElementById("statement").innerHTML = "You Tied! You chose " + playerSelection + " and the computer chose " + computerSelection;
     }
 }
 function computerPlays() {
